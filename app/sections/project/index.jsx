f5dd9823a02ad43d15877e3324424cb6c4ee6615
Muhammad Ansar <109860407+ansar062@users.noreply.github.com>
2024-01-07 00:35:48 +0500

    fix

--- app/sections/project/index.jsx
+++ app/sections/project/index.jsx
@@ -9,7 +9,7 @@ import { ErrorBoundary } from "react-error-boundary";
 import { Projects } from "../../projects/components/Projects";
 import { SITE_ROUTES } from "../../../constants";
 
-const url = `https://ipo27378.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'projects'%20%26%26%20createdAt%20%3E%20'2022-01-01T00%3A00%3A00Z'%5D%20%7B%0A%20%20createdAt%2C%0A%20%20title%2C%0A%20%20description%2C%0A%20%20category%5B%5D-%3E%7Btitle%7D%2C%20%2F%2F%20Fetch%20all%20category%20titles%20as%20an%20array%0A%20%20stack%5B%5D-%3E%7B%0A%20%20%20%20title%0A%20%20%7D%2C%0A%20%20poster%2C%0A%20%20%22images%22%3A%20images%5B%5D.asset-%3Eurl%2C%0A%20%20repoUrl%2C%0A%20%20liveUrl%0A%7D`;
+const url = `${process.env.NEXT_PUBLIC_SANITY_URL}${process.env.NEXT_PUBLIC_SANITY_LATEST_PROJECTS}`;
 
 export function ProjectsSection() {
 	const btnRef = useRef(null);
