dedc7d6f5eb886c484b9a7292416ad659d4beb52
Muhammad Ansar <109860407+ansar062@users.noreply.github.com>
2024-01-07 00:21:22 +0500

    new

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,6 +1,7 @@
 export const fetcher = (url) =>
 	fetch(url, {
 		headers: {
-			Authorization: `Bearer ${process.env.NEXT_PUBLIC_SANITY_TOKEN}`
+			// Authorization: `Bearer ${process.env.NEXT_PUBLIC_SANITY_TOKEN}`
+			Authorization: `Bearer skID6YLzHg8ZwLrAuDf8W2mKLjQtxeVWBER81sGA8x3V824zKRoxc4QRJ3eqc0URXfWZFeS96gOZg86dDn0sXZzQuAtEVLpSAsP1cPDQrQ11BOeJXvCxn1sXSS6JjhBC8UqDFrqpXP5GoPACS60vqBCY3U2icbfKNC2bFuREzC9n0YkGNZ54`
 		}
 	}).then((response) => response.json());
